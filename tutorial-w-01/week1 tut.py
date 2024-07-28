34830b5a7332ba9046fe416b5d99e3c83ffcd0b3
xiangshikelibing <zhefan.chen@uqconnect.edu.au>
2024-07-29 00:22:54 +1000

    week1 tut

--- tutorial-w-01/week1 tut.py
+++ tutorial-w-01/week1 tut.py
@@ -6,10 +6,16 @@ Created on Sun Jul 28 19:14:11 2024
 """
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 
 with open('curve.data', 'r') as i:
     rawdata = list(csv.reader(i,delimiter = ","))
+d =rawdata[1:]
+x = []
+y = []
+for i in d:
+    x.append(float(i[0].split("  ")[0]))
+    y.append(float(i[0].split("  ")[1]))
 
-exampledata = np.array(rawdata[1:])
-
-xdata = exampledata[:,0]
+plt.plot(x, y)
+plt.show()
